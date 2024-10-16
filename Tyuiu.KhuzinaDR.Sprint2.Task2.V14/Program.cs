22b9bfa1e2e25d92e194e78019b611736396f363
angeldrip9919 <i61angel>
2024-10-16 10:09:56 +0500

    Task2

---
+++ Tyuiu.KhuzinaDR.Sprint2.Task2.V14/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.KhuzinaDR.Sprint2.Task2.V14.Lib;
+namespace Tyuiu.KhuzinaDR.Sprint2.Task2.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнила: Хузина Д. Р. | СМАРТб-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Оператор if - полная и короткая форма записи                            *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнила: Хузина Диана Ранидовна | СМАРТб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+            Console.WriteLine("*Написать программу на, которая запрашивает целые значения с клавиатуры и *");
+            Console.WriteLine("* вычисляет находится ли точка с координатами X,Y в заштрихованной области*");
+            Console.WriteLine("*и.                                                                       *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите переменную X");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите переменную Y");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.CheckDotInShadedArea(x, y);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
