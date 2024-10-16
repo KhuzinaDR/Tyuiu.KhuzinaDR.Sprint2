49766e56f37655882c2da2b6d588856cb671b988
angeldrip9919 <i61angel>
2024-10-16 13:30:29 +0500

    Task7

---
+++ Tyuiu.KhuzinaDR.Sprint2.Task7.V11.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Tyuiu.KhuzinaDR.Sprint2.Task7.V11.Lib;
+namespace Tyuiu.KhuzinaDR.Sprint2.Task7.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            double x = -0.5;
+            double y = -0.5;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(res, wait);
+
+        }
+    }
+}
