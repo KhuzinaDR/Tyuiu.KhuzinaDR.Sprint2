a8dc0fceeecad4d58deaf54b180c4677e8d56c5a
angeldrip9919 <i61angel>
2024-10-16 13:02:12 +0500

    Task6

---
+++ Tyuiu.KhuzinaDR.Sprint2.Task6.V1.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.KhuzinaDR.Sprint2.Task6.V1.Lib
+{
+    public class DataService : ISprint2Task6V1
+    {
+        public int FindMonthDaysCount(int value)
+        {
+            int NumbersOfDays;
+            return value switch
+            {
+                1 => NumbersOfDays = 31,
+                2 => NumbersOfDays = 28,
+                3 => NumbersOfDays = 31,
+                4 => NumbersOfDays = 30,
+                5 => NumbersOfDays = 31,
+                6 => NumbersOfDays = 30,
+                7 => NumbersOfDays = 31,
+                8 => NumbersOfDays = 31,
+                9 => NumbersOfDays = 30,
+                10 => NumbersOfDays = 31,
+                11 => NumbersOfDays = 30,
+                12 => NumbersOfDays = 31,
+                < 1 => throw new ArgumentException($"Номер месяца должен быть от 1 до 12. Значение {value} не соответствует условию."),
+                > 12 => throw new ArgumentException($"Номер месяца должен быть от 1 до 12. Значение {value} не соответствует условию."),
+            };
+        }
+    }
+}
---
+++ Tyuiu.KhuzinaDR.Sprint2.Task6.V1/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.KhuzinaDR.Sprint2.Task6.V1.Lib;
+
+namespace Tyuiu.KhuzinaDR.Sprint2.Task6.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнила: Хузина Д. Р. | СМАРТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Получение результата из switch                                    *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Хузина Диана Ранидовна | СМАРТб-24-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует сокращенную форму записи         *");
+            Console.WriteLine("* оператора switch вычисляет требуемое значение и возвращает результат.   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Условие: По данному номеру месяца, определите количество дней в этом    *");
+            Console.WriteLine("* месяце.                                                                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите номер месяца:");
+            int NumberOfMonth = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            if ((NumberOfMonth < 1) || (NumberOfMonth > 12))
+            {
+                Console.WriteLine("Указано неверное значение. Укажите значение от 1 до 12.");
+            }
+
+            else
+            {
+                Console.WriteLine($"Количество дней в месяце : {ds.FindMonthDaysCount(NumberOfMonth)}");
+            }
+        }
+    }
+}
