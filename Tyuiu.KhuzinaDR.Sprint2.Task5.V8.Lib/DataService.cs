456ff8e13e2933aaf80ed877c97144b5189b607e
angeldrip9919 <i61angel>
2024-10-16 12:29:15 +0500

    Task5

---
+++ Tyuiu.KhuzinaDR.Sprint2.Task5.V8.Lib/DataService.cs
@@ -0,0 +1,53 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.KhuzinaDR.Sprint2.Task5.V8.Lib
+{
+    public class DataService : ISprint2Task5V8
+    {
+        public string FindDateOfPreviousDay(int m, int n)
+        {
+            // m и n поменяны местами, из за некорректного написания задания      incorrect
+
+            string res;
+            switch (m)
+            {
+                case 1:
+                    switch (n)
+                    {
+                        case 1:
+                            m = 31;
+                            n = 12;
+                            break;
+                        default:
+                            n = n - 1;
+                            switch (n)
+                            {
+                                case 1:
+                                case 3:
+                                case 5:
+                                case 7:
+                                case 8:
+                                case 10:
+                                    m = 31;
+                                    break;
+                                case 2:
+                                    m = 28;
+                                    break;
+                                default:
+                                    m = 30;
+                                    break;
+                            }
+                            break;
+                    }
+                    break;
+                default:
+                    m = m - 1;
+                    break;
+            }
+            res = ($"{m}.0{n}");  //сам ответ
+            return res;
+
+
+
+        }
+    }
+}
---
+++ Tyuiu.KhuzinaDR.Sprint2.Task5.V8/Program.cs
@@ -0,0 +1,59 @@
+﻿using Tyuiu.KhuzinaDR.Sprint2.Task5.V8.Lib;
+namespace Tyuiu.KhuzinaDR.Sprint2.Task5.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнила: Хузина Д. Р. | СМАРТб-24-1";
+            //Длина строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнила: Хузина Диана Ранидовна | СМАРТб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дата некоторого дня характеризуется двумя натуральными числами: m       *");
+            Console.WriteLine("* (порядковый номер месяца) и n (число). По заданным n и m определить дату*");
+            Console.WriteLine("* предыдущего дня (принять, что n и m не характеризуют 1 января).         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            // incorrect
+            string res;
+
+            Console.WriteLine("Введите месяц: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите день: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+
+
+            if (((n < 1) || (n > 12)) || ((m < 1) || (m > 31)))
+            {
+                res = "Eror 404";
+
+            }
+            else
+            {
+                res = "Предыдущая дата:" + ds.FindDateOfPreviousDay(m, n);
+            }
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+        }
+    }
+}
